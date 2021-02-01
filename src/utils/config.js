f3639b8aad0fe4f45bf1b2a778b47d7c5e511d8e
samsnz <sseneza@gmail.com>
2021-02-01 13:46:32 +0200

    Fix url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3001/api";
-const BASE_URL = "https://aqueous-castle-68448.herokuapp.com//api";
+const BASE_URL = "https://aqueous-castle-68448.herokuapp.com/api";
 
 const config = { BASE_URL };
 
